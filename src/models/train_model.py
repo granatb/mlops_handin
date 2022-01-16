1fe5c3d240d98bd2027fc86536ebc6ad111190a9
granatb <bartek.granat@gmail.com>
2022-01-16 22:13:27 +0100

    added hydra config

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import sys
 
+import hydra
+from omegaconf import OmegaConf
 import model
 import torch
 from model import MyAwesomeModel
@@ -11,27 +13,36 @@ from torch.utils.data import Dataset
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from data.make_dataset import MNISTdata
+import logging
 
-
-def main():
+log = logging.getLogger(__name__)
+print("Working directory : {}".format(os.getcwd()))
+@hydra.main(config_name="training_conf.yaml", config_path="config") # hydra currently supports only 1 config file
+def main(cfg):
+    print(f"configuration: \n {OmegaConf.to_yaml(cfg)}")
+    os.chdir(hydra.utils.get_original_cwd())
+    hparams = cfg.experiment
+    torch.manual_seed(hparams["seed"])
 
     my_model = MyAwesomeModel(128, 10)
     criterion = nn.NLLLoss()
-    optimizer = optim.Adam(my_model.parameters(), lr=0.001)
+    optimizer = optim.Adam(my_model.parameters(), lr=hparams['lr'])
 
     train_data = torch.load("data/processed/train.pth")
     test_data = torch.load("data/processed/test.pth")
 
-    trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-    testloader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
-
-    model.train(my_model, trainloader, testloader, criterion, optimizer, epochs=5)
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=hparams["batch_size"], shuffle=True)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=hparams["batch_size"], shuffle=True)
+    
+    log.info("Start training...")
+    model.train(my_model, trainloader, testloader, criterion, optimizer, epochs=hparams["n_epochs"])
 
     checkpoint = {
         "hidden_size": 128,
         "output_size": 10,
         "state_dict": my_model.state_dict(),
     }
+    log.info("Finish!!")
 
     torch.save(checkpoint, "models/checkpoint.pth")
 
